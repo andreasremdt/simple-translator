033fe9f4f1283663e75bb94568a8f276f4afd740
Andreas Remdt <me@andreasremdt.com>
2020-03-28 14:38:02 +0100

    feat(translator): add cache, split methods

--- src/translator.js
+++ src/translator.js
@@ -3,27 +3,59 @@
 class Translator {
   constructor(options = {}) {
     this._options = Object.assign({}, this.defaultConfig, options);
-    this._lang = this.getLanguage();
+    this._lang = this._detectLanguage();
     this._elements = document.querySelectorAll("[data-i18n]");
+    this._cache = new Map();
   }
 
-  getLanguage() {
+  _detectLanguage() {
     if (!this._options.detectLanguage) {
       return this._options.defaultLanguage;
     }
-    
+
     var stored = localStorage.getItem("language");
 
     if (this._options.persist && stored) {
       return stored;
     }
-    
-    var lang = navigator.languages ? navigator.languages[0] : navigator.language;
+
+    var lang = navigator.languages
+      ? navigator.languages[0]
+      : navigator.language;
 
     return lang.substr(0, 2);
   }
 
-  load(lang = null) {
+  _fetch() {
+    var path = `${this._options.filesLocation}/${this._lang}.json`;
+
+    return new Promise(function(resolve) {
+      fetch(path)
+        .then(response => response.json())
+        .then(resolve)
+        .catch(() => {
+          console.error(
+            `Could not load ${path}. Please make sure that the file exists.`
+          );
+        });
+    });
+  }
+
+  async _getResource() {
+    if (this._cache.has(this._lang)) {
+      return JSON.parse(this._cache.get(this._lang));
+    }
+
+    var translation = await this._fetch();
+
+    if (!this._cache.has(this._lang)) {
+      this._cache.set(this._lang, JSON.stringify(translation));
+    }
+
+    return translation;
+  }
+
+  async load(lang = null) {
     if (lang) {
       if (!this._options.languages.includes(lang)) {
         return;
@@ -32,38 +64,28 @@ class Translator {
       this._lang = lang;
     }
 
-    var path = `${this._options.filesLocation}/${this._lang}.json`;
+    this.translate(await this._getResource());
 
-    fetch(path)
-      .then((response) => response.json())
-      .then((translation) => {
-        this.translate(translation);
-        this.toggleLangTag();
+    document.documentElement.lang = lang;
 
-        if (this._options.persist) {
-          localStorage.setItem("language", this._lang);
-        }
-      })
-      .catch(() => {
-        console.error(`Could not load ${path}. Please make sure that the path is correct.`);
-      });
-  }
-
-  toggleLangTag() {
-    if (document.documentElement.lang !== this._lang) {
-      document.documentElement.lang = this._lang;
+    if (this._options.persist) {
+      localStorage.setItem("language", this._lang);
     }
   }
-  
+
   translate(translation) {
     function replace(element) {
-      var text = element.dataset.i18n.split('.').reduce((obj, i) => obj[i], translation);
-  
+      var text = element.dataset.i18n
+        .split(".")
+        .reduce((obj, i) => obj[i], translation);
+
       if (text) {
         element.innerHTML = text;
       } else {
         element.innerHTML = element.dataset.i18n;
-        console.error(`Could not find text for ${element.dataset.i18n}`);
+        console.warn(
+          `Could not find text for attribute "${element.dataset.i18n}".`
+        );
       }
     }
 
@@ -81,4 +103,7 @@ class Translator {
   }
 }
 
-export default Translator;
+// export default Translator;
+var t = new Translator({
+  languages: ["en", "de"]
+});
